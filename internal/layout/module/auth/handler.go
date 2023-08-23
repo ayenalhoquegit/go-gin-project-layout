7cac6daea8369638d53ea5e2dd0a3f26622f706f
Ayenal hoque <ayenal8684@gmail.com>
2023-08-23 18:13:01 +0600

    modify auth handler

--- internal/layout/module/auth/handler.go
+++ internal/layout/module/auth/handler.go
@@ -44,18 +44,5 @@ func (h *Handler) Login(ctx *gin.Context) {
 	}
 	h.Payload.Id = u.Id
 	tokenString := jwtpkg.GenerateToken(h.Payload)
-	// Create a new token object, specifying signing method and the claims
-	// token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-	// 	"userId": u.Id,
-	// 	"exp":    time.Now().Add(time.Hour * 24).Unix(),
-	// })
-
-	// // Sign and get the complete encoded token as a string using the secret
-	// tokenString, errr := token.SignedString([]byte(config.GetEnvValue("JWT_SECRET")))
-	// if errr != nil {
-	// 	fmt.Println("error", errr)
-	// 	ctx.JSON(http.StatusBadRequest, map[string]string{"error": "Error generation token"})
-	// 	return
-	// }
 	response.Respond(http.StatusCreated, map[string]string{"user": u.Name, "token": tokenString}, ctx)
 }
